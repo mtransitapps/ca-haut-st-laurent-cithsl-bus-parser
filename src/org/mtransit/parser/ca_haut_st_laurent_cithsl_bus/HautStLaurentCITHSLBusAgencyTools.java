2ca4c4cd3339b7d78fe06c2727da1bdf5845c959
Mathieu Méa <mathieu.mea@gmail.com>
2015-01-19 09:34:24 -0500

    Filter on service IDs.

--- src/org/mtransit/parser/ca_haut_st_laurent_cithsl_bus/HautStLaurentCITHSLBusAgencyTools.java
+++ src/org/mtransit/parser/ca_haut_st_laurent_cithsl_bus/HautStLaurentCITHSLBusAgencyTools.java
@@ -1,10 +1,13 @@
 package org.mtransit.parser.ca_haut_st_laurent_cithsl_bus;
 
+import java.util.HashSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.mtransit.parser.DefaultAgencyTools;
 import org.mtransit.parser.Utils;
+import org.mtransit.parser.gtfs.data.GCalendar;
+import org.mtransit.parser.gtfs.data.GCalendarDate;
 import org.mtransit.parser.gtfs.data.GRoute;
 import org.mtransit.parser.gtfs.data.GStop;
 import org.mtransit.parser.gtfs.data.GTrip;
@@ -28,12 +31,39 @@ public class HautStLaurentCITHSLBusAgencyTools extends DefaultAgencyTools {
 		new HautStLaurentCITHSLBusAgencyTools().start(args);
 	}
 
+	private HashSet<String> serviceIds;
+
 	@Override
 	public void start(String[] args) {
 		System.out.printf("Generating CITHSL bus data...\n");
 		long start = System.currentTimeMillis();
+		this.serviceIds = extractUsefulServiceIds(args, this);
 		super.start(args);
 		System.out.printf("Generating CITHSL bus data... DONE in %s.\n", Utils.getPrettyDuration(System.currentTimeMillis() - start));
+	}
+
+	@Override
+	public boolean excludeCalendar(GCalendar gCalendar) {
+		if (this.serviceIds != null) {
+			return excludeUselessCalendar(gCalendar, this.serviceIds);
+		}
+		return super.excludeCalendar(gCalendar);
+	}
+
+	@Override
+	public boolean excludeCalendarDate(GCalendarDate gCalendarDates) {
+		if (this.serviceIds != null) {
+			return excludeUselessCalendarDate(gCalendarDates, this.serviceIds);
+		}
+		return super.excludeCalendarDate(gCalendarDates);
+	}
+
+	@Override
+	public boolean excludeTrip(GTrip gTrip) {
+		if (this.serviceIds != null) {
+			return excludeUselessTrip(gTrip, serviceIds);
+		}
+		return super.excludeTrip(gTrip);
 	}
 
 	@Override
