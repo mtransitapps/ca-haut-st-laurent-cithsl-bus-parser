122c3c5f3ca0bf4a7294d48c070b888ae9a9b73c
Mathieu Méa <mathieu.mea@gmail.com>
2015-07-13 16:59:54 -0400

    Minor improvements...

--- src/org/mtransit/parser/ca_haut_st_laurent_cithsl_bus/HautStLaurentCITHSLBusAgencyTools.java
+++ src/org/mtransit/parser/ca_haut_st_laurent_cithsl_bus/HautStLaurentCITHSLBusAgencyTools.java
@@ -35,7 +35,7 @@ public class HautStLaurentCITHSLBusAgencyTools extends DefaultAgencyTools {
 
 	@Override
 	public void start(String[] args) {
-		System.out.printf("\nGenerating CITHSL bus data...\n");
+		System.out.printf("\nGenerating CITHSL bus data...");
 		long start = System.currentTimeMillis();
 		this.serviceIds = extractUsefulServiceIds(args, this);
 		super.start(args);
